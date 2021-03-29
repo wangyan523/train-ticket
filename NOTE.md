734ca8dec6a47430f8bef419b5e5f55b2bd16de0
wyyyyyy <301059120@qq.com>
2021-03-29 22:16:54 +0800

    lazy，suspense，purecomponent，memo

--- NOTE.md
+++ NOTE.md
@@ -3,3 +3,16 @@
 - Consumer 是子组件使用 用来接收父组件的数据
 - Context 可以嵌套传值
 - ContextType 简化了组件接收值
+
+### import动态导入文件
+- eg: import(/* webpackChunkName:"xxx" */'xxx.js').then(...)
+
+### lazy需要和suspense一起使用
+- suspense是加载空隙显示，fallback需要显示jsx
+- suspense无法加载错误文件，需要使用ErrorBoundary，捕获错误，需要在componentDidCatch生命钩子中使用
+
+### 子组件渲染优化
+- shouldComponentUpdate 可以优化子组件多次渲染
+- PureComponent 也可以优化子组件的多次渲染，但是只有传入的props第一级发生变化才会渲染变化，注意使用场景
+- 函数式组件可以使用memo包裹
+- PureComponent和memo都只能做浅层对比，使用的是shallowEqual算法
--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,24 @@
-import { Component, createContext, useState } from 'react'
+import { Component, memo } from 'react'
 import './App.css';
 
-const BatteryContext = createContext()
-const OnlineContext = createContext()
-
-class Leaf extends Component {
-  static contextType = BatteryContext
-
+const Foo = memo(function Foo () {
+  console.log('Foo render')
+  return <div></div>
+})
+class App extends Component {
+  state = {
+    count: 0
+  }
   render () {
-    const battery = this.context
     return (
-      <h1>Battery: {battery}</h1>
+      <div>
+        <button onClick={() => { this.setState({ count: this.state.count + 1 }) }}>Add</button>
+        <Foo name="Mike" />
+        {this.state.count}
+      </div>
     )
   }
 }
 
-function Middle () {
-  return (
-    < Leaf />
-  )
-}
-
-function App () {
-  const [battery, setBattery] = useState(60)
-  const [online, setOnline] = useState(false)
-  return (
-    <BatteryContext.Provider value={battery}>
-      <OnlineContext.Provider value={online}>
-        <button onClick={() => { setBattery(battery + 1) }}> Press</button>
-        <Middle />
-        <button onClick={() => { setOnline(!online) }}> Minus</button>
-      </OnlineContext.Provider>
-    </BatteryContext.Provider>
-  );
-}
 
 export default App;
